ef764ff57f987a3ec0b38dd64d6b2d6f7578d62e
Konrad <gren.konrad@gmail.com>
2017-05-22 07:44:21 +0200

    Setup a gulp watch for css

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,2 @@
-var gulp = require('gulp'),
-    browserSync = require('browser-sync').create();
-
-gulp.task('watch', function() {
-  browserSync.init({
-    server: {
-      baseDir: "./app"
-    }
-  });
-});
-
-watch()
+require('./gulp/tasks/watch');
+require('./gulp/tasks/styles');
